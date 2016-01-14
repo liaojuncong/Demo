010e870cb2da5f8f3f9d5f47a9dd494a4d366860
LiaoJunCong <liaojuncong@foxmail.com>
2016-01-14 13:16:32 +0800

    Orleans

---
+++ Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using Orleans;
+using Sample.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            GrainClient.Initialize();
+
+            while (true)
+            {
+                Console.WriteLine("请输入用户的手机号码来判断是否存在：");
+                //var mobileNumber = Console.ReadLine();
+
+                for (var i = 0; i < 10000; i++)
+                {
+                    //Task.Run(() =>
+                    {
+                        var mobileNumber = i.ToString();
+                        var userService = GrainClient.GrainFactory.GetGrain<IUserGrain>(mobileNumber);
+                        Console.WriteLine($"用户{mobileNumber}，{(userService.Exist().Result ? "已经存在" : "还不存在")}");
+                    }
+                    //);
+                }
+                if (Console.ReadLine() == "exit")
+                    GrainClient.Uninitialize();
+            }
+        }
+    }
+}
---
+++ Sample.Interfaces/IUserGrain.cs
@@ -0,0 +1,14 @@
+﻿using Orleans;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample.Interfaces
+{
+    public interface IUserGrain : IGrainWithStringKey
+    {
+        Task<bool> Exist();
+    }
+}
